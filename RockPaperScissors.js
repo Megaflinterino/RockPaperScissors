e0cae26bf5497e988efc40e060a7e021d8117a07
Megaflinterino <steven.p.rudolf@outlook.de>
2024-11-25 13:36:11 +0100

    Added The function getComputerChoice which picks a random number between 0 and 100 Then chooses Rock paper or scissors corrosponding to the value of the number. Basically a random generator for Rock, paper or Scissors.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+
+
+function getComputerChoice(){
+    let a = (Math.random() * 100) + 1;
+
+    if (a <= 33){
+        console.log("Rock")
+    }
+
+    else if (a > 34 && a < 66){
+        console.log("Paper")
+    }
+
+    else if (a > 66){
+        console.log("Scissors")
+    }
+
+}
+
+console.log(getComputerChoice())
