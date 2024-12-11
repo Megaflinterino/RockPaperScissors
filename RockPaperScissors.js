8ec62066f67576a4ce7d4af381bf430d72346535
Megaflinterino <steven.p.rudolf@outlook.de>
2024-12-11 11:56:35 +0100

    Added the function for players to input their choice(getHumanChoice) and made getComputerChoice a little more accurate.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,20 +1,38 @@
 
+let humanScore = 0
+let cumputerScore = 0
 
 function getComputerChoice(){
     let a = (Math.random() * 100) + 1;
 
-    if (a <= 33){
-        console.log("Rock")
+    console.log(a)
+
+    if (a <= 33.333){
+        let computerChoice = "Rock"
+        console.log(computerChoice)
     }
 
-    else if (a > 34 && a < 66){
-        console.log("Paper")
+    else if (a > 33.333 && a < 66.666){
+        let computerChoice = "Paper"
+        console.log(computerChoice)
     }
 
-    else if (a > 66){
-        console.log("Scissors")
+    else if (a > 66.666){
+        let computerChoice = "Scissors"
+        console.log(computerChoice)
     }
+}
+//getHumanchoice noch case insensitive machen :rOcK PAPER scissORs
+function getHumanChoice(){
+    let humanChoice = prompt("Rock, Paper or Scissors?");
+    console.log(humanChoice)
 
+    if (humanChoice !== "Rock" && humanChoice !== "Paper" && humanChoice !== "Scissors"){
+        alert("Please Input only Rock, Paper or Scissors!")
+    }
 }
 
+function playRound(humanChoice, computerChoice){}
+
 console.log(getComputerChoice())
+console.log(getHumanChoice())
