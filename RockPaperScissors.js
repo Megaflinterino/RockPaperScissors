d211c33b270f3068ffe0fa2e2500524059991dc0
Megaflinterino <steven.p.rudolf@outlook.de>
2025-01-07 20:55:27 +0100

    Moved the aquiring of the values for human and computer choice in the playRound function. PlayGame still isnt working. But i will improve it

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -9,26 +9,22 @@ function getComputerChoice(){
 
     if (a <= 33.333){
         let computerChoice = "rock"
-        console.log(computerChoice)
         return "rock"
     }
 
     else if (a > 33.333 && a < 66.666){
         let computerChoice = "paper"
-        console.log(computerChoice)
         return "paper"
     }
 
     else if (a > 66.666){
         let computerChoice = "scissors"
-        console.log(computerChoice)
         return "scissors"
     }
 }
 //getHumanchoice noch case insensitive machen :rOcK PAPER scissORs
 function getHumanChoice(){
     let humanChoice = prompt("Rock, Paper or Scissors?");
-    console.log(humanChoice)
 
     if (humanChoice.toLowerCase() !== "rock" && humanChoice.toLowerCase() !== "paper" && humanChoice.toLowerCase() !== "scissors"){
         alert("Please Input only Rock, Paper or Scissors!")
@@ -39,6 +35,10 @@ function getHumanChoice(){
 
 //Funktioniert nicht, gibt zwar die ersten logs aus aber die if schleifen lösen nicht aus, nächstes mal beheben.
 function playRound(humanChoice, computerChoice){
+
+
+    console.log(humanChoice)
+    console.log(computerChoice)
 
     if (humanChoice === "rock" && computerChoice === "rock"){
         console.log("Even!")
@@ -91,6 +91,7 @@ function playRound(humanChoice, computerChoice){
 }
 
 function playGame(playRound){
+
     for (let i = 0; i < 5; i++){
         playRound(humanSelection, computerSelection)
     }
@@ -99,4 +100,4 @@ function playGame(playRound){
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
 
-    playGame(playRound)
+playGame(playRound)
